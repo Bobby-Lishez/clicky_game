d0c136365a5408135eea296a6be4d4131844a148
Bobby-Lishez <right2bob@msn.com>
2018-06-26 19:23:30 -0600

    icon

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 const Footer = () => (
     <footer className = "footer">
         <div className = "bottom">
-            This is a very necessarry footer <img alt = "react" src = "/images/favicon.ico" />
+            This is a very necessarry footer <img alt = "react" src = "./images/favicon.ico" />
         </div>
     </footer>
 );
